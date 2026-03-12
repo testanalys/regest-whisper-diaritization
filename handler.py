c1a02233a9708940e102f362fbac1e3007e1e060
otrier <119528528+testanalys@users.noreply.github.com>
2026-03-12 01:56:51 -0400

    Create handler.py

---
+++ handler.py
@@ -0,0 +1,24 @@
+import runpod
+from predict import Predictor
+
+predictor = Predictor()
+predictor.setup()
+
+def handler(job):
+    job_input = job["input"]
+    output = predictor.predict(
+        file_string=job_input.get("file_string"),
+        file_url=job_input.get("file_url"),
+        file=job_input.get("file"),
+        num_speakers=job_input.get("num_speakers"),
+        translate=job_input.get("translate", False),
+        language=job_input.get("language"),
+        prompt=job_input.get("prompt"),
+    )
+    return {
+        "segments": output.segments,
+        "language": output.language,
+        "num_speakers": output.num_speakers,
+    }
+
+runpod.serverless.start({"handler": handler})
