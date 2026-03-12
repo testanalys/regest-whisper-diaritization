b4352ecd74ccfc514e607221350ed907f74794e2
otrier <119528528+testanalys@users.noreply.github.com>
2026-03-11 22:56:30 -0400

    Update predict.py

--- predict.py
+++ predict.py
@@ -35,7 +35,7 @@ class Predictor(BasePredictor):
         )
         self.diarization_model = Pipeline.from_pretrained(
             "pyannote/speaker-diarization-3.1",
-            use_auth_token="YOUR HF TOKEN",
+            use_auth_token=os.getenv('HF_token'),
         ).to(torch.device("cuda"))
 
     def predict(
